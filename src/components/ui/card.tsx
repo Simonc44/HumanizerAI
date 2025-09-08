840d18060f7e1dead2815ba2c12c9299afcc56cc
Simon Chusseau <wehipi8051@leabro.com>
2025-09-08 18:43:59 +0000

    ## Error Type Console Error
    ## Error Message
    Cannot access telemetryLab

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -31,25 +31,29 @@ const CardHeader = React.forwardRef<
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLHeadingElement>) => (
+const CardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
   <h3
+    ref={ref}
     className={cn("text-2xl font-semibold leading-none tracking-tight", className)}
     {...props}
   />
-)
+))
+CardTitle.displayName = "CardTitle"
 
-const CardDescription = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLParagraphElement>) => (
+const CardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
   <p
+    ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
-)
+))
+CardDescription.displayName = "CardDescription"
 
 const CardContent = React.forwardRef<
   HTMLDivElement,
